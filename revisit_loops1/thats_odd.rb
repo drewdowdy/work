dc740b4a84b8643cb60cb27c91a5c2bdab01fbd4
Drew D <drewdowdy109@gmail.com>
2023-12-06 15:55:43 -0500

    created and updated thats_odd.rb

---
+++ revisit_loops1/thats_odd.rb
@@ -0,0 +1,9 @@
+=begin
+
+Problem: Modify the code so that it only outputs `i` if `i` is an odd number
+
+=end
+
+for i in 1..100
+  puts i if i.odd? # add a contidion to output if `i.odd?` is truthy
+end
