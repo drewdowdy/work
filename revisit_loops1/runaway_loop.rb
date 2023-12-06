c7f4b9a8442ae212cc7ff89eafa66845bdc5d514
Drew D <drewdowdy109@gmail.com>
2023-12-06 14:40:03 -0500

    created revisit_loops1 directory and runaway_loop.rb

---
+++ revisit_loops1/runaway_loop.rb
@@ -0,0 +1,10 @@
+=begin
+
+Problem: Modify the code so that the loop stops after the first iteration
+
+=end
+
+loop do
+  puts 'Just keep printing...'
+  break # use the keyword `break` to exit the loop
+end
