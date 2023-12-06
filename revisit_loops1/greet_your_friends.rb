6987d3c8bde53fc61f98e5d39f32527935f8cfb0
Drew D <drewdowdy109@gmail.com>
2023-12-06 15:58:39 -0500

    created and updated greet_your_friends.rb

---
+++ revisit_loops1/greet_your_friends.rb
@@ -0,0 +1,18 @@
+=begin
+
+Problem: Given the following array of names, use a for loop to greet each friend individually.
+
+Examples:
+
+Hello, Sarah!
+Hello, John!
+Hello, Hannah!
+Hello, Dave!
+
+=end
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends
+  puts "Hello, #{friend}!"
+end
