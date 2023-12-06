9bddf2ca138a4314e3a36aecfc0b20827d1a4967
Drew D <drewdowdy109@gmail.com>
2023-12-06 15:31:54 -0500

    created and updated count_up.rb

---
+++ revisit_loops1/count_up.rb
@@ -0,0 +1,12 @@
+=begin
+
+Problem: Modify the code so that it counts from 1 to 10
+
+=end
+
+count = 1        # change `count` to 1
+
+until count > 10 # when `count` is bigger than 10, it stops
+  puts count
+  count += 1     # increment up instead of down
+end
