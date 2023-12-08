07f1aa5a8b0613159512eaaf8c08860237ad5ba8
Drew D <drewdowdy109@gmail.com>
2023-12-08 10:43:25 -0500

    updated greeting.rb

---
+++ revisit_loops2/greeting.rb
@@ -0,0 +1,16 @@
+=begin
+
+ Problem: Use a while loop to print "Hello!" twice.
+
+=end
+
+def greeting
+  puts 'Hello!'
+end
+
+number_of_greetings = 2
+
+while number_of_greetings != 0
+  greeting
+  number_of_greetings -= 1
+end
