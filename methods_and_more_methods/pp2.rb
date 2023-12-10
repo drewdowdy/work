46483a7765f5d0679c0e77527ee8f9505796ff77
Drew D <drewdowdy109@gmail.com>
2023-12-10 09:59:30 -0500

    updated pp2.rb

--- methods_and_more_methods/pp2.rb
+++ methods_and_more_methods/pp2.rb
@@ -2,6 +2,8 @@
 
 Problem: How does count treat the block's return value? How can we find out?
 
+Answer: We can check the Ruby docs and see that `#count` returns the number of elements that make the block evaluate as `true`. This means that `#count` considers the truthiness of the block.
+
 =end
 
 count = ['ant', 'bat', 'caterpillar'].count do |str|
@@ -10,5 +12,3 @@ end
 
 p count
 # => 2
-
-
