002f067562876e202fd897e6daefca50b873891d
Drew D <drewdowdy109@gmail.com>
2024-01-11 14:49:36 -0500

    cleared test.rb

--- test.rb
+++ test.rb
@@ -1,23 +1,2 @@
 # Paste some code here and execute it. When finished, erase it.
 
-customer_orders = [
-  {
-    customer_id: 12,
-    customer_name: 'Emma Lopez',
-    orders: [
-      { order_fulfilled: true, order_value: 135.99 },
-      { order_fulfilled: true, order_value: 289.49 },
-      { order_fulfilled: false, order_value: 58.00 }
-    ]
-  },
-  {
-    customer_id: 32,
-    customer_name: 'Michael Richards',
-    orders: [
-      { order_fulfilled: true, order_value: 120.00 },
-      { order_fulfilled: false, order_value: 85.65 }
-    ]
-  },
-  # rest of data...
-]
-
