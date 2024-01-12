340ab05f777bd6863a5af43c2d9aeee1a1772c5c
Drew D <drewdowdy109@gmail.com>
2024-01-12 14:11:01 -0500

    created 'joinor' method

--- lesson_3/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe.rb
@@ -42,10 +42,21 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(array, separator = ', ', last_word = 'or')
+  case array.size
+  when 0 then ''
+  when 1 then array.first.to_s
+  when 2 then array.join( " #{last_word} ")
+  else
+    array[-1] = "#{last_word} #{array.last}"
+    array.join(separator) 
+  end
+end
+
 def player_marks!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
 
