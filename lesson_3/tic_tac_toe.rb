7741dac97c1f7599ac9879dd5a53d5321838bce1
Drew D <drewdowdy109@gmail.com>
2024-01-16 16:44:22 -0500

    refactored 'main game loop' with new methods

--- lesson_3/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe.rb
@@ -56,51 +56,12 @@ def joinor(array, separator = ', ', last_word = 'or')
   end
 end
 
-def player_marks!(brd)
-  square = ''
-  loop do
-    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
-    square = gets.chomp.to_i
-    break if empty_squares(brd).include?(square)
-
-    prompt "Sorry, that's not a valid choice."
-  end
-
-  brd[square] = PLAYER_MARKER
-end
-
 def ideal_square(line, brd, marker)
   if brd.values_at(*line).count(marker) == 2
     brd.select { |k,v| line.include?(k) && v == INITIAL_MARKER }.keys.first
   else
     nil
   end
-end
-
-def computer_marks!(brd)
-  square = nil
-
-  WINNING_LINES.each do |line|
-    square = ideal_square(line, brd, COMPUTER_MARKER)  # offense
-    break if square                                  
-  end
-
-  if !square
-    WINNING_LINES.each do |line|      
-      square = ideal_square(line, brd, PLAYER_MARKER)  # defense
-      break if square                                  
-    end
-  end
-
-  if !square
-    square = 5 if empty_squares(brd).include?(5) # chooses square 5 if available
-  end
-
-  if !square
-    square = empty_squares(brd).sample # chooses randomly if no offense, defense, or 5
-  end
-
-  brd[square] = COMPUTER_MARKER
 end
 
 def board_full?(brd)
@@ -122,6 +83,51 @@ def detect_winner(brd)
   nil
 end
 
+def mark_board!(brd, current_player)
+  if current_player == 'p'
+    square = ''
+
+    loop do
+      prompt "Choose a square (#{joinor(empty_squares(brd))}):"
+      square = gets.chomp.to_i
+      break if empty_squares(brd).include?(square)
+
+      prompt "Sorry, that's not a valid choice."
+    end
+
+    brd[square] = PLAYER_MARKER
+  elsif current_player == 'c'
+    square = nil
+
+    WINNING_LINES.each do |line|
+      square = ideal_square(line, brd, COMPUTER_MARKER)  # offense
+      break if square                                  
+    end
+
+    if !square
+      WINNING_LINES.each do |line|      
+        square = ideal_square(line, brd, PLAYER_MARKER)  # defense
+        break if square                                  
+      end
+    end
+
+    if !square
+      square = 5 if empty_squares(brd).include?(5) # chooses square 5 if available
+    end
+
+    if !square
+      square = empty_squares(brd).sample # chooses randomly if no offense, defense, or 5
+    end
+
+    brd[square] = COMPUTER_MARKER
+  end
+end
+
+def alternate_player(current_player)
+  return 'c' if current_player == 'p'
+  return 'p' if current_player == 'c'
+end
+
 player_score = 0
 computer_score = 0
 
@@ -136,21 +142,21 @@ loop do
 
   sleep 0.5
 
-  first_player = nil
+  current_player = nil
 
   loop do                                                                   # decide the first player
     prompt "Who should go first? 'Player', 'Computer', or 'I don't care'? (p/c/idc)"
-    first_player = gets.chomp
+    current_player = gets.chomp
     
-    break if first_player.downcase.start_with?('p') || first_player.downcase.start_with?('c') || first_player.downcase == 'idc'
+    break if current_player.downcase.start_with?('p') || current_player.downcase.start_with?('c') || current_player.downcase == 'idc'
 
     prompt "Please type 'p' for Player, 'c' for Computer or 'idc' for I don't care."
 
     sleep 0.5
   end
   
-  if first_player.downcase == 'idc'
-    first_player = ['p','c'].sample
+  if current_player.downcase == 'idc'
+    current_player = ['p','c'].sample
   end
 
   loop do                 # main game loop
@@ -160,23 +166,10 @@ loop do
     puts "Player: #{player_score} Computer: #{computer_score}".center(40)
     puts '=' * 40
 
-    if first_player.downcase.start_with?('p')           # player first loop
-      player_marks!(board)
-      break if someone_won?(board) || board_full?(board)
-
-      display_board(board)
-
-      computer_marks!(board)
-      break if someone_won?(board) || board_full?(board)
-    elsif first_player.downcase.start_with?('c')         # computer first loop
-      computer_marks!(board)
-      break if someone_won?(board) || board_full?(board)
-
-      display_board(board)
-
-      player_marks!(board)
-      break if someone_won?(board) || board_full?(board)
-    end
+    mark_board!(board, current_player)
+    current_player = alternate_player(current_player)
+    
+    break if someone_won?(board) || board_full?(board)
   end
 
   display_board(board)
@@ -199,9 +192,9 @@ loop do
   break if player_score == 5 || computer_score == 5 
 
   answer = nil
-  
+
   loop do
-    prompt "Continue? (y/n)"
+    prompt "Continue? (y/n)"            # `continue?` prompt after each round
     answer = gets.chomp
     break if answer.downcase.start_with?('n')
 
