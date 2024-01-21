3dc0dc3f52d206fc617fd66d6be0c37e2e25a847
Drew D <drewdowdy109@gmail.com>
2024-01-21 00:47:40 -0500

    finalized twenty_one.rb

--- lesson_3/twenty-one.rb
+++ lesson_3/twenty-one.rb
@@ -1,17 +1,3 @@
-=begin
-
-1. Initialize deck
-2. Deal cards to player and dealer
-3. Player turn: hit or stay
-  - repeat until bust or "stay"
-4. If player bust, dealer wins.
-5. Dealer turn: hit or stay
-  - repeat until total >= 17
-6. If dealer bust, player wins.
-7. Compare cards and declare winner.
-
-=end
-
 require 'pry'
 
 def prompt(msg)
@@ -30,48 +16,39 @@ def initialize_deck
     all_cards["jack of #{suit}"] = 10
     all_cards["queen of #{suit}"] = 10
     all_cards["king of #{suit}"] = 10
-    all_cards["ace of #{suit}"] = [1, 11]
+    all_cards["ace of #{suit}"] = 11
   end
 
   all_cards
 end
 
-def deal_card(deck)
-  card = initialize_deck.keys.sample
+def deal_card!(deck)
+  card = deck.sample
   deck.delete(card)
   card
 end
 
-def total(values, hand)
+def total(hand)
+  values = initialize_deck
   total = 0
   hand.each do |key|
-    total += values[key]
+    num = values[key]
+    total += num
   end
 
-  hand.select{|card| card.include?('ace')}.count.times do 
-    sum -= 10 if total > 21
+  if total > 21
+    hand.select{|card| card.include?('ace')}.count.times do # makes 'ace' worth 1 if total is over 21
+      total -= 10
+    end
   end
 
   total
 end
 
-def joiner(array, separator = ', ', last_word = 'and')
-  case array.size
-  when 0 then array.join
-  when 1 then array.join
-  when 2 then array.join( " #{last_word} ")
-  else
-    array[-1] = "#{last_word} #{array.last}"
-    array.join(separator) 
-  end
-end
-
 def display_greeting
   puts '=' * 60
-  puts 'Welcom to Twenty-One!'.center(60)
+  puts 'Welcome to Twenty-One!'.center(60)
   puts '=' * 60
-  puts "To see the rules, press 'r'."
-  puts "To play the game, press 'p'."
 end
 
 def display_rules
@@ -97,7 +74,20 @@ def display_rules
   puts 'Dealer\'s turn:'
   puts '  - They will choose to hit or stay based on their hand.'
   puts ''
-  puts 'Do you want to reset or play? (r/p)'
+  puts 'Do you want to quit or play? (q/p)'
+end
+
+def who_wins?(player_hand, dealer_hand)
+  player_total = total(player_hand)
+  dealer_total = total(dealer_hand)
+
+  if player_total > 21 || (21 - player_total).abs > (21 - dealer_total).abs
+    return 'Dealer'
+  elsif dealer_total > 21 || (21 - dealer_total).abs > (21 - player_total).abs
+    return 'Player'
+  elsif player_total == dealer_total
+    return 'Everyone'
+  end
 end
 
 # ======================================================================
@@ -107,6 +97,8 @@ end
 system 'clear'
 
 display_greeting
+prompt "To see the rules, press 'r'."
+prompt "To play the game, press 'p'."
 answer = nil
 
 loop do 
@@ -117,45 +109,84 @@ loop do
     display_rules
     answer = gets.chomp.downcase
   end
-  break if answer == 'p'
-  puts "Please type 'p' or 'r'."
+  break if answer == 'p' || answer == 'q'
+  puts "Please type 'q' to quit or 'p' to play."
 end
 
-card_values = initialize_deck
-
-deck = initialize_deck.keys
-
-player_hand = [deal_card(deck), deal_card(deck)]
-dealer_hand = [deal_card(deck), deal_card(deck)]
-
-prompt "Dealer has: #{dealer_hand[0]} and unknown card."
-prompt "You have: #{player_hand[0]} and #{player_hand[1]}."
-
-move = nil
-winner = nil
-
 loop do # main game loop
+  break if answer == 'q'
+
+  card_values = initialize_deck
+  deck = initialize_deck.keys
+
+  player_hand = [deal_card!(deck), deal_card!(deck)]
+  dealer_hand = [deal_card!(deck), deal_card!(deck)]
+
+  prompt "Dealer has: #{dealer_hand[0]} and unknown card."
+  prompt "You have: #{player_hand[0]} and #{player_hand[1]}."
+
   loop do # Player Turn
+    move = nil
     loop do 
       prompt 'What will you do now? Hit or Stay? (h/s)'
       move = gets.chomp.downcase
 
       break if move == 'h' || move == 's'
       prompt "Please type 'h' for hit or 's' for stay."
-      sleep 0.5
+      sleep(0.5)
     end
-    break if move == 's'
 
-    player_hand << deal_card(deck) if move == 'h'
-    prompt "Now, you have #{joiner(player_hand)}."
+    break if move == 's'
+    sleep(0.5)
+
+    player_hand << deal_card!(deck) if move == 'h'
+    prompt "Now, you have #{player_hand.join(', ')}."
   end
+
+  prompt "Now it's the dealer's turn!"
 
   loop do # Dealer Turn
-
-    break if total(card_values, dealer_hand) >= 17
+    break if total(dealer_hand) >= 17
+    dealer_hand << deal_card!(deck)
+    prompt 'The dealer chose to hit!'
+    sleep 0.5
   end
 
-  break
+  prompt 'The dealer chose to stay!'
+
+  sleep(0.5)
+
+  prompt ''
+  prompt "Dealer has #{dealer_hand.join(', ')}."
+  prompt "You have #{player_hand.join(', ')}."
+  prompt ''
+
+  sleep(0.5)
+
+  if total(dealer_hand) > 21
+    prompt "Dealer has #{total(dealer_hand)} and busted!"
+  elsif total(player_hand) > 21
+    prompt "You have #{total(player_hand)} and busted!"
+  else
+    prompt "You have #{total(player_hand)} and the dealer has #{total(dealer_hand)}."
+  end
+
+  winner = who_wins?(player_hand, dealer_hand)
+
+  prompt "#{winner} is the winner!"
+
+  continue = nil
+
+  loop do # Play again?
+    prompt 'Do you want to play again? (y/n)'
+    continue = gets.chomp.downcase
+    break if continue == 'n' || continue == 'y'
+    
+    prompt "Please type 'y' for yes and 'n' for no."
+  end
+
+  break if continue == 'n'
 end
 
+sleep(0.5)
 prompt 'Thanks for playing Twenty-One. Goodbye!'
